6a1f75d068e210a4e90103fb7fc774e6b0d59308
hugodu69 <hugogogo@protonmail.com>
2020-04-08 14:27:07 +0200

    permiere version du site

---
+++ header.js
@@ -0,0 +1,38 @@
+var header = document.getElementById("header");
+var content_header = '' +
+			'<div id="name" class="horizontal">' +
+				'<a class="title" href="#">greve2loyers</a>' +
+			'</div>' +
+			'<nav id="nav_menu" class="horizontal">' +
+				'<div>' +
+					'<a class="title menu" href="#">guide</a>' +
+				'</div>' +
+				'<div>' +
+					'<a class="title menu" href="#">données</a>' +
+					'<div class="submenu">' +
+						'<a class="title menu" href="#">sociologiques</a>' +
+						'<a class="title menu" href="#">juridiques</a>' +
+					'</div>' +
+				'</div>' +
+				'<div>' +
+					'<a class="title menu" href="#">temoignages</a>' +
+				'</div>' +
+				'<div>' +
+					'<a class="title menu" href="#">references</a>' +
+					'<div class="submenu">' +
+						'<a class="title menu" href="#">historiques</a>' +
+						'<a class="title menu" href="#">actuelles</a>' +
+					'</div>' +
+				'</div>' +
+				'<div><a class="title menu" href="#">ressources</a>' +
+					'<div class="submenu">' +
+						'<a class="title menu" href="#">exemples de lettres</a>' +
+						'<a class="title menu" href="#">tracts</a>' +
+						'<a class="title menu" href="#">images</a>' +
+					'</div>' +
+				'</div>' +
+				'<div>' +
+					'<a class="title menu" href="#">f.a.q.</a>' +
+				'</div>' +
+			'</nav>';
+header.innerHTML=content_header;
