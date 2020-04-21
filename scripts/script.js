06e273a5c8c5dd6b59b2a60651c1737d1401b1be
greve2loyers <1greve2loyers@protonmail.com>
2020-04-21 13:16:58 +0200

    description

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 insert_html("./elements/header.html", "header");
 insert_html("./elements/footer.html", "footer");
-let _desc = "ce site relaie l'appel à la grève qui apparaît partout dans le monde depuis le début de la crise sanitaire du covid19, en ajoutant ses propres ressources et en listant le plus possibles les autres appels et ressources proposées ailleurs sur internet";
-_desc = "test test"
-insert_description(_desc);
 
 function insert_html(text_from, insert_to) {
 	fetch(text_from)
@@ -12,9 +9,5 @@ function insert_html(text_from, insert_to) {
 		.then(data => {
 			document.querySelector(insert_to).innerHTML = data;
 		});
-}
-
-function insert_description(_desc) {
-	document.querySelector('meta[name="description"]').setAttribute("content", _desc);
 }
 
